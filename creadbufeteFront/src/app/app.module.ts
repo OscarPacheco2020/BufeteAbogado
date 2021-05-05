8d4d30a9e23aaeffd9b33aef350353e1eeb6aa1a
OscarPacheco2020 <oscar.rene.pc@gmail.com>
2021-05-05 15:20:37 -0600

    Programacion del servicio cliente

--- creadbufeteFront/src/app/app.module.ts
+++ creadbufeteFront/src/app/app.module.ts
@@ -3,15 +3,19 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
-// extartna 
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-
-import { ToastrModule } from 'ngx-toastr';
 import { ListaClienteComponent } from './cliente/lista-cliente.component';
 import { DetalleClienteComponent } from './cliente/detalle-cliente.component';
 import { NuevoClienteComponent } from './cliente/nuevo-cliente.component';
 import { EditarClienteComponent } from './cliente/editar-cliente.component';
+
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+
+
+// extartna 
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ToastrModule } from 'ngx-toastr';
+
 
 @NgModule({
   declarations: [
@@ -25,7 +29,10 @@ import { EditarClienteComponent } from './cliente/editar-cliente.component';
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule, // required animations module
-    ToastrModule.forRoot() //
+    ToastrModule.forRoot(), //
+    HttpClientModule,
+    FormsModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
--- creadbufeteFront/src/app/service/cliente.service.ts
+++ creadbufeteFront/src/app/service/cliente.service.ts
@@ -1,9 +1,35 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Cliente } from '../models/cliente';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ClienteService {
 
-  constructor() { }
+  clienteURL = 'http://localhost:8080/cliente';
+  
+  constructor(private httpClient: HttpClient) { }
+
+  public list(): Observable<Cliente[]> {
+    return this.httpClient.get<Cliente[]>(this.clienteURL + '/');
+  }
+
+  public detail(id: number): Observable<Cliente> {
+    return this.httpClient.get<Cliente>(this.clienteURL + `/${id}` );
+  }
+
+  public sava(cliente: Cliente): Observable<any> {
+    return this.httpClient.post<any>(this.clienteURL + '/', cliente);
+  }
+
+  public update(id: number, cliente: Cliente): Observable<any> {
+    return this.httpClient.put<any>(this.clienteURL + `/${id}` , cliente);
+  }
+
+  public delete(id: number): Observable<any>{
+    return this.httpClient.delete<any>(this.clienteURL + `/${id}`);
+  }
+
 }
